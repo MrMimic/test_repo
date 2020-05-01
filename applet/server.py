ff41aa392b25c45e2d346b757e2a62444e137ed7
emeric.dynomant@gmail.com <emeric.dynomant@gmail.com>
2020-05-01 10:38:29 +0200

    Add Flask example

--- applet/server.py
+++ applet/server.py
@@ -1,16 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+import plotly
+from plotly import graph_objs as go
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
+def create_plot(args_1):
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    try:
+        N = int(args_1)
+    except ValueError:
+        N = 40
+
+    x = np.random.uniform(low=0, high=10, size=(N, ))
+    y = np.random.uniform(low=0, high=10, size=(N, ))
+
+    df = pd.DataFrame({'x': x, 'y': y})
+
+
+    data = [
+        go.Scatter(
+            x=df['x'],
+            y=df['y'],
+            mode='markers'
+        )
+    ]
+
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
+
+
+@app.route('/', methods=['POST'])
+def updated_index():
+    args_1 = request.form['user_input']
+    bar = create_plot(args_1)
+    return render_template('index.html', plot=bar)
 
 
 @app.route('/about')
-def cakes():
+def about():
     return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- package/library.py
+++ package/library.py
@@ -1,11 +1,9 @@
 def sum(a: int, b: int) -> int:
     """
     Small test function.
-
     Args:
         a (int): First value.
         b (int): Second value.
-
     Returns:
         Sum a + b.
     """
